f3356703d7584fe61ab7fef9e123b08c0a2ccd6e
emaus-debug <kouakouemausivan@gmail.com>
2022-03-09 15:33:22 +0000

    Register fonctionnality Username and Email validation config

---
+++ Wallet/static/js/register.js
@@ -0,0 +1,52 @@
+const username = document.querySelector('#username');
+const feedBackArea = document.querySelector('.invalid_feedback');
+const email = document.querySelector('#email');
+const emailFeedBackArea = document.querySelector('.emailFeedBackArea');
+
+email.addEventListener("keyup", (e) => {
+    const emailVal = e.target.value;
+
+    email.classList.remove("is-invalid");
+    emailFeedBackArea.style.display = "none";
+
+    if (emailVal.length > 0){
+        fetch('/authapp/validate-email', 
+        {
+            body : JSON.stringify({ email: emailVal }),
+            method : "POST",
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, 
+        }).then((res) =>res.json())
+        .then((data)=>{
+            console.log('data',data);
+            if (data.email_error){
+                email.classList.add("is-invalid");
+                emailFeedBackArea.style.display = "block";
+                emailFeedBackArea.innerHTML = data.email_error
+            }
+        });
+    }
+});
+
+username.addEventListener("keyup", (e) => {
+    const usernameVal = e.target.value;
+
+    username.classList.remove("is-invalid");
+    feedBackArea.style.display = "none";
+
+    if (usernameVal.length > 0){
+        fetch('/authapp/validate-username', 
+        {
+            body : JSON.stringify({ username: usernameVal }),
+            method : "POST",
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, 
+        }).then((res) =>res.json())
+        .then((data)=>{
+            console.log('data',data);
+            if (data.username_error){
+                username.classList.add("is-invalid");
+                feedBackArea.style.display = "block";
+                feedBackArea.innerHTML = data.username_error
+            }
+        });
+    }
+});
--- authapp/views.py
+++ authapp/views.py
@@ -1,7 +1,37 @@
+import email
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from validate_email import validate_email
 
 # Create your views here.
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error' : 'Email invalid ' }, status=400)
+        if User.objects.filter(email = email).exists() : 
+            return JsonResponse({'email_error' : 'Désolé cet email est déja utilisé, veuillez en choisir un autre'}, status=409)
+
+        return JsonResponse({'email_valid': True})
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error' : 'Le nom d\'utilisateur doit contenir uniquement que des caractères alphanumérique' }, status=400)
+        if User.objects.filter(username = username).exists() : 
+            return JsonResponse({'username_error' : 'Désolé cet nom d\'utilisateur existe déjà veuillez en choisir un autre'}, status=409)
+
+        return JsonResponse({'username_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
